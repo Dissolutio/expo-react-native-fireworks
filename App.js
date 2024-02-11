08a9e030f8684ee00c976bda7950c1e70e0b92ef
John M <entity.john@gmail.com>
2024-02-11 16:05:08 -0600

    add some radial-gradient components, but not what we are looking for yet

--- App.js
+++ App.js
@@ -3,14 +3,15 @@ import { StyleSheet, Text, View } from "react-native";
 import { Banner } from "./Banner";
 import { colors } from "./colors";
 import { LinearGradientBackground } from "./LinearGradient";
+import { RadialGradientBackground } from "./RadialGradient";
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
-        <LinearGradientBackground>
+        <RadialGradientBackground>
           <Banner />
-        </LinearGradientBackground>
+        </RadialGradientBackground>
       </View>
       <StatusBar style="auto" />
     </>
