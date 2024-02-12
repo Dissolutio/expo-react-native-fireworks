86d2b2d1e6c97acc816367f69c1a0d3d53a20e71
John M <entity.john@gmail.com>
2024-02-11 23:40:40 -0600

    copy gradient stops over from glowcard

--- RadialGradient.js
+++ RadialGradient.js
@@ -6,36 +6,49 @@ const HUE_SPEED = 1;
 class RadialGradientBg extends React.Component {
   render() {
     const { children, innerColor, hueValue } = this.props;
+    const currentHue = hueValue * HUE_SPEED;
     const windowWidth = Dimensions.get("window").width;
     const windowHeight = Dimensions.get("window").height;
-    const colorHsl = `hsl(${hueValue * HUE_SPEED}, 100%, 60%)`;
+    const innerHsl = `hsl(${currentHue}, 100%, 90%)`;
+    const secondHsl = `hsl(${currentHue}, 100%, 80%)`;
+    const colorHsl = `hsl(${currentHue}, 100%, 60%)`;
     return (
       <View style={{ flex: 1 }}>
         <Svg height="100%" width="100%">
           <Defs>
             <RadialGradient
               id="grad"
-              cx={windowWidth / 2}
-              cy={windowHeight / 2}
-              rx={Math.max(windowWidth, windowHeight) / 2}
-              ry={Math.max(windowWidth, windowHeight) / 2}
+              cx={"50%"}
+              cy={"50%"}
+              rx={"30%"}
+              ry={"30%"}
               // fx={0}
               // fy={0}
               gradientUnits="userSpaceOnUse"
             >
+              <Stop offset="0" stopColor={innerHsl ?? "#fff"} stopOpacity="1" />
               <Stop
-                offset="0"
-                stopColor={innerColor ?? "#fff"}
+                offset="0.2"
+                stopColor={secondHsl ?? "#fff"}
                 stopOpacity="1"
               />
-              <Stop offset="1" stopColor={colorHsl ?? "#000"} stopOpacity="1" />
+              <Stop
+                offset="0.4"
+                stopColor={colorHsl ?? "#000"}
+                stopOpacity="1"
+              />
+              <Stop offset="1" stopColor={"transparent"} stopOpacity="1" />
             </RadialGradient>
           </Defs>
           <Ellipse
-            cx={windowWidth / 2}
-            cy={windowHeight / 2}
-            rx={Math.max(windowWidth, windowHeight) / 2}
-            ry={Math.max(windowWidth, windowHeight) / 2}
+            // cx={windowWidth / 2}
+            // cy={windowHeight / 2}
+            cx={"50%"}
+            cy={"50%"}
+            // rx={Math.max(windowWidth, windowHeight) / 2}
+            // ry={Math.max(windowWidth, windowHeight) / 2}
+            rx={"100%"}
+            ry={"100%"}
             fill="url(#grad)"
           />
         </Svg>
