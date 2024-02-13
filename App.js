95208bb34d79e91c6b4743a13170692993c7abc9
John M <entity.john@gmail.com>
2024-02-13 16:09:51 -0600

    add rotation to the strobing gradient, this is the laser for the card

--- App.js
+++ App.js
@@ -1,38 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Animated, View, Easing } from "react-native";
-import { useRef, useEffect } from "react";
-import { Banner } from "./Banner";
+import { StyleSheet, View } from "react-native";
 import { colors } from "./colors";
-import { LinearGradientBackground } from "./LinearGradient";
-import { RadialGradientBackground } from "./RadialGradient";
+import { AnimatedRadialGradient } from "./RadialGradient";
 
-const hueInterpolation = {
-  inputRange: [0, 1],
-  outputRange: [0, 360],
-};
 export default function App() {
-  const outerColor = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    Animated.loop(
-      Animated.timing(outerColor, {
-        toValue: 1,
-        useNativeDriver: false,
-        duration: 4000,
-        // easing: Easing.inOut(Easing.exp),
-      })
-    ).start();
-  }, []);
-
   return (
     <>
       <View style={styles.container}>
-        <RadialGradientBackground
-          innerColor={colors.white}
-          hueValue={outerColor.interpolate(hueInterpolation)}
-        >
-          <Banner />
-        </RadialGradientBackground>
+        <AnimatedRadialGradient />
       </View>
       <StatusBar style="auto" />
     </>
--- RadialGradient.js
+++ RadialGradient.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { View, Dimensions, Animated } from "react-native";
+import { View, Dimensions, Animated, Easing } from "react-native";
 import Svg, { Defs, RadialGradient, Stop, Ellipse } from "react-native-svg";
 
 const HUE_SPEED = 1;
+const GLOW_SCALE = 1.5;
+const GLOW_RADIUS = 100;
+const ANIMATION_DURATION = 4000;
 class RadialGradientBg extends React.Component {
   render() {
-    const { children, innerColor, hueValue } = this.props;
+    const { hueValue, gradientPosX, gradientPosY } = this.props;
     const currentHue = hueValue * HUE_SPEED;
     const windowWidth = Dimensions.get("window").width;
     const windowHeight = Dimensions.get("window").height;
@@ -18,8 +21,10 @@ class RadialGradientBg extends React.Component {
           <Defs>
             <RadialGradient
               id="grad"
-              cx={"50%"}
-              cy={"50%"}
+              // cx={"50%"}
+              // cy={"50%"}
+              cx={`${gradientPosX}%`}
+              cy={`${gradientPosY}%`}
               rx={"30%"}
               ry={"30%"}
               // fx={0}
@@ -52,10 +57,56 @@ class RadialGradientBg extends React.Component {
             fill="url(#grad)"
           />
         </Svg>
-        {children}
       </View>
     );
   }
 }
 export const RadialGradientBackground =
   Animated.createAnimatedComponent(RadialGradientBg);
+
+const hueInterpolation = {
+  inputRange: [0, 1],
+  outputRange: [0, 360],
+};
+const gradientPosXInterpolation = {
+  inputRange: [0, 0.25, 0.5, 0.75, 1],
+  outputRange: [0, 100, 100, 0, 0],
+};
+const gradientPosYInterpolation = {
+  inputRange: [0, 0.25, 0.5, 0.75, 1],
+  outputRange: [0, 0, 100, 100, 0],
+};
+export const AnimatedRadialGradient = () => {
+  const outerColor = React.useRef(new Animated.Value(0)).current;
+  const gradientPos = React.useRef(new Animated.Value(0)).current;
+
+  React.useEffect(() => {
+    Animated.loop(
+      Animated.timing(outerColor, {
+        toValue: 1,
+        useNativeDriver: false,
+        duration: (ANIMATION_DURATION * 2) / Math.PI, // to give us a non-repetitive strobe
+        // easing: Easing.inOut(Easing.exp),
+        easing: Easing.linear,
+      })
+    ).start();
+    Animated.loop(
+      Animated.timing(gradientPos, {
+        toValue: 1,
+        useNativeDriver: false,
+        duration: ANIMATION_DURATION,
+        easing: Easing.linear,
+      })
+    ).start();
+  }, []);
+
+  return (
+    <>
+      <RadialGradientBackground
+        hueValue={outerColor.interpolate(hueInterpolation)}
+        gradientPosX={gradientPos.interpolate(gradientPosXInterpolation)}
+        gradientPosY={gradientPos.interpolate(gradientPosYInterpolation)}
+      />
+    </>
+  );
+};
